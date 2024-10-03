ba600015df711737b30a7b992434d95d194d9553
harish@git <ssharishleo007@gmail.com>
2024-10-03 15:51:45 +0530

    Initial commit

---
+++ README.txt
@@ -0,0 +1,16 @@
+MEMORY GAME TASK (Flip cards)
+
+  About Game:
+This Flip cards memory is about finding the matching pairs by fliping the hidden cards.
+
+  Display:
+1.This game has 8 pair of cards so total 16 cards in display.
+2.In the 4 X 4 format it has 4 rows of card (4 in each row).
+
+  Usage:
+1.Game board has 16 shuffled cards deck with main value hidden behind each of the cards
+2.Just click cards to see what symbol behind it and try to find the same symbol from other cards.
+3.If both the cards does not match then both the selected cards flip back.
+4.If both the cards does match they you found a pair and cards remain face-up.
+5.Find 8 matching pars of cards to win the game.
+6.Click restart button to play game again,Have fun!
---
+++ index.js
@@ -0,0 +1,114 @@
+let score = 0;
+let gameOn = false;
+const buttonEle = document.getElementById('restart-btn');
+
+const cardsArray = [
+  'A', 'A', 'B', 'B', 'C', 'C', 'D', 'D',
+  'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H'
+];
+
+const designObj = {
+  'A': '<i class="fa fa-star" aria-hidden="true"></i>',
+  'B': '<i class="fa fa-heart" aria-hidden="true"></i>',
+  'C': '<i class="fa fa-play" aria-hidden="true"></i>',
+  'D': '<i class="fa fa-diamond" aria-hidden="true"></i>',
+  'E': '<i class="fa fa-arrow-left" aria-hidden="true"></i>',
+  'F': '<i class="fa fa-arrow-up" aria-hidden="true"></i>',
+  'G': '<i class="fa fa-arrow-right" aria-hidden="true"></i>',
+  'H': '<i class="fa fa-arrow-down" aria-hidden="true"></i>',
+};
+
+let firstCard = null;
+let secondCard = null;
+
+// shuffle the initial cardsArray
+function shuffle(array) {
+  return array.sort(() => Math.random() - 0.5);
+}
+
+function createCard(cardValue) {
+  const card = document.createElement('div');
+  card.classList.add('card');
+  
+  const front = document.createElement('div');
+  front.classList.add('front');
+  
+  const back = document.createElement('div');
+  back.classList.add('back');
+  back.setAttribute("data-value",cardValue);
+  back.innerHTML = designObj[cardValue];
+
+  card.appendChild(front);
+  card.appendChild(back);
+  
+  card.addEventListener('click', flipIt);
+  
+  return card;
+}
+
+// flip the cards 
+function flipIt() {
+  if (gameOn || this === firstCard) return;
+  this.classList.toggle('flipped');
+
+  if (!firstCard) {
+    firstCard = this;
+    return;
+  }
+  secondCard = this;
+  isMatching();
+
+  if (score===8){
+    setTimeout(() => {
+      alert("Congrats You've WON,Click Restart to Play Again");
+      buttonEle.classList.add('game-over');
+    }, 500);
+  }
+}
+
+// find if selected cards match 
+function isMatching() {
+  gameOn = true;
+  const firstValue = firstCard.querySelector('.back').dataset.value;
+  const secondValue = secondCard.querySelector('.back').dataset.value;
+
+  if (firstValue === secondValue) {
+    score += 1
+    firstCard.removeEventListener('click', flipIt);
+    secondCard.removeEventListener('click', flipIt);
+    resetCards();
+    return;
+  }
+
+  setTimeout(() => {
+    firstCard.classList.remove('flipped');
+    secondCard.classList.remove('flipped');
+    resetCards();
+  }, 1000);
+}
+
+function resetCards() {
+  [firstCard, secondCard] = [null, null];
+  gameOn = false;
+}
+
+// Start the game 
+function startGame() {
+  buttonEle.classList.remove('game-over');
+  const shuffledCards = shuffle(cardsArray);
+  let mainGameEle = document.querySelector("#main");
+  console.log(shuffledCards);
+  shuffledCards.forEach(cardValue => {
+    const card = createCard(cardValue);
+    mainGameEle.appendChild(card);
+  });
+}
+
+//  restart the game 
+buttonEle.addEventListener('click', () => {
+  document.getElementById('main').innerHTML = '';
+  score = 0;
+  startGame();
+});
+
+startGame();
